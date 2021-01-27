7873a9da76e0c1c319044cc24abe0bbf44d98bf9
a-ebra <a-ebra@users.noreply.github.com>
2021-01-26 22:11:34 -0700

    updated axios get requests and dispatch

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -58,14 +58,15 @@ export default function useApplicationData(props) {
 
   useEffect(() => {
     Promise.all([
-      axios.get("/api/days/"),
-      axios.get("/api/appointments/"),
-      axios.get("/api/interviewers/")
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then(all => {
       dispatch({
         type: SET_APPLICATION_DATA,
         days: all[0].data,
-    
+        appointments: all[1].data,
+        interviewers: all[2].data
       });
     });
   }, []);
