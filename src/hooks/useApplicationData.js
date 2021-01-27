79e56bfa053d520b2ac91565347260349030a412
a-ebra <a-ebra@users.noreply.github.com>
2021-01-26 22:11:03 -0700

    tests for integration, updated spots count

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,79 @@
+import { useEffect, useReducer } from "react";
+import axios from "axios";
+
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
+
+export default function useApplicationData(props) {
+  const [state, dispatch] = useReducer(reducer, {
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => dispatch({ type: SET_DAY, day: day });
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios
+      .put(`/api/appointments/${id}`, appointment)
+      .then(() => {
+          return dispatch({
+            type: SET_INTERVIEW,
+            appointments
+          })
+        }
+      );
+  }
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios
+      .delete(`/api/appointments/${id}`, appointment)
+      .then(() =>
+        dispatch({
+          type: SET_INTERVIEW,
+          appointments
+        })
+      );
+  }
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days/"),
+      axios.get("/api/appointments/"),
+      axios.get("/api/interviewers/")
+    ]).then(all => {
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        days: all[0].data,
+    
+      });
+    });
+  }, []);
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  };
+}
